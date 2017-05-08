2347a3be726fa54ce80da28ef9ee2b89deefbdcb
adnanrimedzo <adnanrimedzo@hotmail.com>
2017-05-08 13:10:24 +0300

    read img added

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='sony.watermarking',
-    version='0.1.0',
+    version='0.1.1',
     description='Watermarking core application for Sony',
     long_description=readme,
     author='Adnan Ozdemir',
